11e243dec66551a4f2566c1af411d399dcb9fc02
Yann Michel LE COZ <yann.lecoz@ynov.com>
2019-01-24 16:01:18 +0100

    UPDATE 24/01/2019
    Mise en ligne du Site Statique sur un serveur local.

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
+import http.server
+
+PORT = 8080
+server_address = ("", PORT)
+
+server = http.server.HTTPServer
+handler = http.server.CGIHTTPRequestHandler
+handler.cgi_directories = ["./dossier_site_statique"]
+print("Serveur actif sur le port :", PORT)
+
+httpd = server(server_address, handler)
+httpd.serve_forever()
+
+
 changed_lines = None
+number_asterix = 0
 
 # Écriture dans le fichier html_file.html
-with open("./file_generated.html", "w") as sorted_file:
+with open("./dossier_site_statique/file_generated.html", "w") as sorted_file:
     # Interprétation des caractères spéciaux
     sorted_file.write("""<meta charset="UTF-8"/>\n\n""")
     # Lecture du fichier test_file.md
     with open("./input_file.md") as open_file:
         for lines in open_file:
             lines = "  " + lines
-            print(lines)
             # Convertion des titres Markdown
             if lines.startswith("  # ") == True:
                 changed_lines = lines.replace("  # ", "<h1>")
@@ -26,4 +40,18 @@ with open("./file_generated.html", "w") as sorted_file:
                 changed_lines = changed_lines.replace("\n", "</h3>\n")
                 sorted_file.write(changed_lines)
             else:
-                sorted_file.write(lines)
+                # Conversion de la mise en gras
+                for element in lines:
+                    if element == "*":
+                        number_asterix += 1
+                    else:
+                        number_asterix
+                if number_asterix == 2:
+                    changed_lines = lines.replace(" *", "<strong>")
+                    changed_lines = lines.replace("*", "</strong>")
+                    sorted_file.write(changed_lines)
+                else:
+                    changed_lines = lines.replace("*", "<li>")
+                    sorted_file.write(changed_lines)
+                # else:
+                #    sorted_file.write(lines)
