76f83437d895f7e5dc09bc48f7f3e93d991e3ab6
Yann LE COZ <yann.lecoz@ynov.com>
2019-02-10 15:03:33 +0100

    UPDATE 10/02/2018
    Finalisation du Site Statique

--- README.md
+++ README.md
@@ -11,5 +11,14 @@ Ce projet permet de générer une page HTML à partir d'un fichier Markdown.
 * Markdown2
 * Argparse
 
+## Manuel d'Utilisation
+Pour utiliser l'application, il vous faudra tout d'abord installer différentes librairies:
+* **Markdown2 :** `pip install markdown2`
+* **Argparse :** `pip install argparse`
+
+Ensuite ouvrez dans *une invite de commande* le fichier `main.py` puis ajoutez lui les commandes suivantes:
+* `-i` ou `--input-directory` suivi du nom du fichier **Markdown** que vous souhaitez convertir ;
+* `-o` ou `--output-directory` suivi du nom du fichier **HTML**.
+
 ## Licence
 MIT License
--- main.py
+++ main.py
@@ -1,53 +1,38 @@
-from markdown2 import Markdown
+# Importation des librairies
+import markdown2
 import argparse
 
-parser = argparse.ArgumentParser(
-    description="Convert a markdown folder to an HTML folder"
-)
-parser.add_argument(
-    "-i",
-    "--input",
+def convert_MdFile_to_HTMLFile(inputFile, outputFile):
+    """
+    Fonction gérant la conversion des fichiers Markdown en fichiers HTML
+    """
+    # Création et écriture du fichier HTML
+    with open(f"./{outputFile}.html", "w") as output_file:
+        # Ouverture du fichier et lecture du fichier Marckdown
+        with open(f"./{inputFile}", "r") as input_file:
+            # Ajout de l'en-tête
+            output_file.write("<!DOCTYPE html>\n<html>\n<head>\n\t<meta charset='utf-8'/>\n\t<title>MDtoHTML</title>\n\t<link rel='stylesheet' type='text/css' href='main.css'/>\n</head>\n<body>\n")
+            # Conversion du fichier Markdown en HTML
+            for lines in input:
+                output_file.write(markdown2.markdown(lines))
+            # Fermeture des balises "body" et "/html"
+            output_file.write("</body>\n</html>")
+
+print("Générateur de site statique.\n")
+
+# Création du manuel de l'application
+parser = argparse.ArgumentParser(description="Convert a markdown folder to an HTML folder")
+parser.add_argument("-i",
+    "--input-directory",
     help="open the folder containing the markdown files to convert",
-    action="store_true",
-)
-parser.add_argument(
-    "-o",
-    "--output",
+    action="store_true",)
+parser.add_argument("-o",
+    "--output-directory",
     help="create the folder containing the files converted to HTML",
-    action="store_true",
-)
+    action="store_true",)
 args = parser.parse_args()
-if args.input:
-    print("Open folder is '{}'".format(__file__))
-if args.output:
-    print("Create folder is '{}'".format(__file__))
 
-md = Markdown()
+print("Les fichiers Markdown ont bien été convertis en HTML.")
 
-# Écriture dans le fichier HTML
-with open("./index.html", "w") as exit_file:
-    exit_file.write("<meta charset='UTF-8'/>\n")
-    # Lecture du fichier Markdown
-    with open("./input_file.md", "r") as open_file:
-        for lines in open_file:
-            print(lines)
-            # Gestion des titres indentés
-            if lines.startswith("  ##") == True:
-                lines = lines.replace("  ## ", "<h2>")
-                lines = lines.replace("\n", "</h2>\n")
-            # Gestion des liens URL
-            if (
-                lines.startswith("http") == True
-                or lines.startswith("* http") == True
-            ):
-                lines = lines.replace("http", '<a href="http')
-                lines = lines.replace("\n", '">hyperlien</a>\n')
-            elif lines.startswith("un texte") == True:
-                exit_file.write("<p>")
-                lines = lines.replace("http", '<a href="http')
-                lines = lines.replace(
-                    ".com et encore du texte",
-                    '.com">hyperlien</a> et encore du texte</p>',
-                )
-            lines = md.convert(lines)
-            exit_file.write(lines)
+# Appel de la fonction de conversion
+convert_MdFile_to_HTMLFile(args.input, args.output)
